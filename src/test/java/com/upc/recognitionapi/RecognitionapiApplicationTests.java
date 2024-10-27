2827f67d3ab1b92afc2ed339323b26ebf7319534
Raul Ventura <rventura@sanms.com.pe>
2024-10-27 14:00:48 -0500

    Initial commit

---
+++ src/test/java/com/upc/recognitionapi/RecognitionapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.recognitionapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecognitionapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
